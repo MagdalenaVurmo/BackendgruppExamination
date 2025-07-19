f7e79e75d473cfa7c024bc0e86c45c210c79ac3d
Karlgit13 <karl.vareskog@gmail.com>
2025-07-19 12:38:57 +0200

    karl .env....

--- Controllers/InfoController.js
+++ Controllers/InfoController.js
@@ -9,10 +9,10 @@ export const getCompanyInfo = async (req, res) => {
     try {        // Startar ett try/catch-block för att hantera eventuella fel.
         const companyInfo = await fetchCompanyInfo();
         res.status(200).json(companyInfo)              // Skickar tillbaka det hämtade svaret till klienten som ett JSON-objekt med HTTP-statuskoden 200 
-                                                       // Om det inte finns någon info i databasen, skickas en tom array.     
+        // Om det inte finns någon info i databasen, skickas en tom array.     
 
     } catch (error) {              // Fångar alla fel som kan uppstå i try-blocket, t.ex. om fetchCompanyInfo() misslyckas.
-        console.error( 'Fel vid hämtning av företagsinfo: ', error);
+        console.error('Fel vid hämtning av företagsinfo: ', error);
         res.status(500).send('Fel vid hämtning av företagsinfo')
         // Den här skickar tillbaka ett felmeddelande till klienten med HTTP-statuskoden 500 (ett internt serverfel).
     }
